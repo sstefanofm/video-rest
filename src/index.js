5b31c34d01eede20b41c32dc5dc031fab2f85fb8
stf <sstefanofm@gmail.com>
2024-07-16 08:23:46 -0300

    empty get & post

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const app = require('express')()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.post('/', (req, res) => {
+  res.sendStatus(201)
+})
+
+app.listen(PORT, () => {
+  console.log('Video-Rest is listening on port ' + PORT)
+})
