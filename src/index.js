c70198ffb20001ddd2dcac1b95e3e9fadfc22792
stf <sstefanofm@gmail.com>
2024-07-16 10:05:03 -0300

    saving video to Disk with unique name and identifier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 require('dotenv').config()
 
+const multer = require('multer')
+
 const app = require('express')()
 const PORT = process.env.PORT || 3000
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'media/')
+  },
+  filename: (req, file, cb) => {
+    const uniqSuffix = Date.now() + '-' + Math.floor(Math.random() * 1e4)
+
+    cb(null, `${ file.fieldname }-${ uniqSuffix }.mp4`)
+  }
+})
+const upload = multer({ storage })
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
 })
 
-app.post('/', (req, res) => {
-  res.sendStatus(201)
+app.post('/', upload.single('video'), (req, res) => {
+  res.status(201).send(req.file.filename)
 })
 
 app.listen(PORT, () => {
