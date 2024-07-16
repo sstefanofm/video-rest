14bbdebef3ef07a849b89daaaca6529e6b3476e6
stf <sstefanofm@gmail.com>
2024-07-16 11:47:23 -0300

    validate all files mimeTypes before saving

--- src/index.js
+++ src/index.js
@@ -12,16 +12,46 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const uniqSuffix = Date.now() + '-' + Math.floor(Math.random() * 1e4)
 
-    cb(null, `${ file.fieldname }-${ uniqSuffix }.mp4`)
+    cb(null, `${ file.fieldname }-${ uniqSuffix }.avi`)
+  },
+})
+const upload = multer({
+  storage,
+  /* validation for all files */
+  fileFilter: (req, file, cb) => {
+    const ext = Object.freeze({
+      AVI: '.avi',
+      MP4: '.mp4'
+    })
+
+    const availableMimes = {
+      'video/mp4': ext.MP4,
+      'video/x-mp4': ext.MP4,
+      'video/avi': ext.AVI,
+      'video/x-avi': ext.AVI,
+      'video/vnd.avi': ext.AVI,
+      'video/x-msvideo': ext.AVI,
+    }
+
+    const validMime = mimeType => {
+      return availableMimes.hasOwnProperty(mimeType)
+    }
+
+    if (!validMime(file.mimetype))
+      return cb(null, false)
+
+    cb(null, true)
   }
 })
-const upload = multer({ storage })
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
 })
 
 app.post('/', upload.single('video'), (req, res) => {
+  if (!req.file)
+    return res.status(400).send('Hubo un error al subir el video.')
+
   res.status(201).send(req.file.filename)
 })
 
